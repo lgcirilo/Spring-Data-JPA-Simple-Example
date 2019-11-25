14094ba79ce37f3125700badba6b1211ba824b86
cyfa <cyfa@petrobras.com.br>
2019-11-25 17:05:40 -0300

    first commit.

---
+++ src/main/java/dev/lgcirilo/SpringDataJPASimpleExample/SpringDataJpaSimpleExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.lgcirilo.SpringDataJPASimpleExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaSimpleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaSimpleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lgcirilo/SpringDataJPASimpleExample/SpringDataJpaSimpleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lgcirilo.SpringDataJPASimpleExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaSimpleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
